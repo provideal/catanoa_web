1fb3901d014e0f7ad6b25a58dedaba587f6b66dc
Torsten Sprenger <ts@metaminded.com>
2012-09-25 18:13:04 +0200

    nivo slider integrated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require 'jquery.nivo.slider'
+//= require 'jquery.nivo.slider.pack'
+//= require 'nivo-slider'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,9 @@ module ApplicationHelper
     css = (options[:item] == @navigation_item ? 'active' : '')
     css << ' ' + options[:class] if options[:class].present?
     css.strip!
+    opts = css.present? ? {:class => css} : {}
     label = options[:label] || ''
-    content_tag(:li, :class => css) do
+    content_tag(:li, opts) do
       concat(link_to(label.html_safe, options[:path]) + (block_given? ? yield : ''))
     end
   end
