177fab1f6642c3de2b144af70fd1f9dd63ed4fc6
Torsten Sprenger <ts@metaminded.com>
2012-10-01 16:27:15 +0200

    some images

--- app/assets/javascripts/nivo-slider.js
+++ app/assets/javascripts/nivo-slider.js
@@ -1,11 +1,11 @@
 jQuery(function($){
   $('#slider').nivoSlider({
-    effect: 'random', // Specify sets like: 'fold,fade,sliceDown'
+    effect: 'fold', // Specify sets like: 'fold,fade,sliceDown'
     slices: 15, // For slice animations
     boxCols: 8, // For box animations
     boxRows: 4, // For box animations
-    animSpeed: 500, // Slide transition speed
-    pauseTime: 4000, // How long each slide will show
+    animSpeed: 300, // Slide transition speed
+    pauseTime: 5000, // How long each slide will show
     startSlide: 0, // Set starting Slide (0 index)
     directionNav: true, // Next & Prev navigation
     controlNav: false, // 1,2,3... navigation
